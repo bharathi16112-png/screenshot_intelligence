c770db7373066ad740d608c7323e48dff00cf5f1
Bharathi <2782187@tcsgenailab.onmicrosoft.com>
2026-06-15 06:36:35 +0530

    fix: remove duplicate catchall file, keep only api/index.py

--- api/[...catchall].py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'backend'))
-
-from main import app
-from mangum import Mangum
-
-handler = Mangum(app, lifespan="off")
